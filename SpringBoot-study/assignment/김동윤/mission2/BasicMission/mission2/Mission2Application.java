bcd6800f5bc5df03d70843809f4305cbd2c7ec51
myway00 <junior0614@naver.com>
2022-02-22 23:39:34 +0900

    pgl/김동윤 : 두번째 미션 수행

---
+++ SpringBoot-study/assignment/김동윤/mission2/BasicMission/mission2/Mission2Application.java
@@ -0,0 +1,13 @@
+package jsbdy.mission2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mission2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mission2Application.class, args);
+	}
+
+}

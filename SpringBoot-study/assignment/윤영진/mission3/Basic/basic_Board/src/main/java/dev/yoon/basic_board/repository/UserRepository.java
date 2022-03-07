80c55f26e3721ca4798d37dc96e5c0bac2a32100
yoon <dudwls143@gmail.com>
2022-03-07 20:42:41 +0900

    peer/윤영진:Chapter6 내용 정리 & 세번째 미션 수행

--- SpringBoot-study/assignment/윤영진/mission3/Basic/basic_Board/src/main/java/dev/yoon/basic_board/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.yoon.basic_board.repository;
-
-import dev.yoon.basic_board.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User,Long> {}
--- SpringBoot-study/assignment/윤영진/mission3/Challenge/challenge_Board/src/main/java/dev/yoon/basic_board/domain/Category.java
+++
@@ -1,5 +0,0 @@
-package dev.yoon.basic_board.domain;
-
-public enum Category {
-    FOOD, CLOTHES, SHOES
-}
